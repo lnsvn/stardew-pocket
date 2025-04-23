a3f0cbff5fb6e3c628a24fcaf9cfc549606df292
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2025-04-23 18:40:18 -0400

    removes react & vite placeholders from App.jsx, adds a fixed Footer component, and imports the Footer component to App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+      <>
+        <p className="fixed bottom-0 right-0 left-0 py-5 bg-sky-300">
+          This site is a fan-made tool and is not affiliated with or endorsed by
+          ConcernedApe or Chucklefish. Stardew Valley is a trademark of
+          ConcernedApe.
+        </p>
+      </>
+    );
+  }
+  
+  export default Footer;
