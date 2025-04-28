f603f7c61b4023812c2e7041c0c42c6994a02766
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2025-04-27 22:50:03 -0400

    created pages dir, created Landing Page and imported to App.jsx, deleted placeholder text from App.jsx, removes padding and dark mode settings from App and index.css respectively, and deletes react.svg

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,32 @@
+function LandingPage() {
+  return (
+    <>
+      <h1 className="p-20">Images</h1>
+      <p className="px-10 pb-10 font-bold">
+        Welcome to Stardew Valley Pocket Companion!
+      </p>
+      <p className="px-10 pb-10">
+        Your journey to 100% perfection just got a whole lot easier — and a lot
+        more fun! This companion app is designed to help you track your
+        progress, stay organized, and enjoy every moment of your adventure in
+        Stardew Valley.
+      </p>
+      <p className="px-10 pb-10">
+        Easily keep tabs on friendships, Community Center bundles, special
+        orders, collections, and every milestone on your way to completing the
+        game. Need a reminder of who loves which gifts? Wondering what crops
+        you’re missing for the greenhouse? We’ve got you covered with helpful
+        information and quick tips right at your fingertips.
+      </p>
+      <p className="px-10 pb-10">
+        Whether you’re a brand new farmer or a seasoned pro, Stardew Valley
+        Pocket Companion makes it simple and satisfying to see your hard work
+        pay off. Track, plan, and celebrate your farm life — one step closer to
+        perfection!
+      </p>
+      <p className="px-10 pb-10 font-bold">Start your journey today!</p>
+    </>
+  );
+}
+
+export default LandingPage;
